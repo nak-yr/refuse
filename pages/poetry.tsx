ea2ec51b6a21bb077944bb099553975f3ebd667e
nak-yr <nkwork874@gmail.com>
2022-06-17 21:30:09 +0900

    issue#14 Poetryページのコンテンツ一覧雛形作成

--- pages/poetry.tsx
+++ pages/poetry.tsx
@@ -1,13 +1,35 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import GearIcon from "@rsuite/icons/Gear";
+import { List, FlexboxGrid, Placeholder } from "rsuite";
 
 import styles from "../styles/Home.module.css";
 import "rsuite/dist/rsuite.min.css";
 
 import Header from "./components/header";
 
+interface PoetryEntry {
+  date: string;
+  title: string;
+}
+
+const dummyEntry: PoetryEntry[] = [
+  {
+    date: "yyyy-mm-dd",
+    title: "test content1",
+  },
+  {
+    date: "yyyy-mm-dd",
+    title: "test content2",
+  },
+  {
+    date: "yyyy-mm-dd",
+    title: "test content3",
+  },
+];
+
 const Poetry: NextPage = () => {
+  const { Paragraph } = Placeholder;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -22,11 +44,23 @@ const Poetry: NextPage = () => {
       <Header />
 
       <div className={styles.main}>
-        <b>
-          <GearIcon spin />
-          under construction
-          <GearIcon spin />
-        </b>
+        <List hover style={{ width: "90%" }}>
+          {dummyEntry.map((dummy, index) => (
+            <List.Item key={index}>
+              <FlexboxGrid>
+                <FlexboxGrid.Item colspan={6}>
+                  {/* タイトルだけ親要素のフォントサイズの1.5倍で表示する */}
+                  <div style={{ fontSize: "1.5em" }}>{dummy.title}</div>
+                  <div>{dummy.date}</div>
+                </FlexboxGrid.Item>
+                <FlexboxGrid.Item colspan={18}>
+                  {/* Prop `style` did not match が出るけど、Paragraphはテストにしか使わないので一旦無視で */}
+                  <Paragraph />
+                </FlexboxGrid.Item>
+              </FlexboxGrid>
+            </List.Item>
+          ))}
+        </List>
       </div>
     </div>
   );
