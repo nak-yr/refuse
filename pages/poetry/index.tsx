9a897dbf3258e018d39471cd363b4f0ed3291193
nak-yr <nkwork874@gmail.com>
2023-03-26 18:23:21 +0900

    issue#31 Poetry一覧ページの大まかなレイアウト変更

--- pages/poetry/index.tsx
+++ pages/poetry/index.tsx
@@ -1,12 +1,17 @@
+/** @jsxImportSource @emotion/react */
+
 import type { NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import { List, FlexboxGrid } from "rsuite";
+import { useRouter } from "next/router";
 
-import styles from "../../styles/Home.module.css";
-import "rsuite/dist/rsuite.min.css";
+import { css } from "@emotion/css";
+import { IconButton } from "rsuite";
+import ArrowLeftLineIcon from "@rsuite/icons/ArrowLeftLine";
 
 import Header from "../components/header";
+import React, { useEffect } from "react";
+import { handleClientScriptLoad } from "next/script";
+import Link from "next/link";
 
 interface PoetryEntry {
   date: string;
@@ -50,54 +55,106 @@ const entry: PoetryEntry[] = [
 ];
 
 const Poetry: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Poetry</title>
-        <meta
-          name="description"
-          content="A website created with the desire to be an artist."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const router = useRouter();
+  const toHome = () => {
+    router.push("/");
+  };
 
-      <Header />
-      <div className={styles.main}>
-        <List style={{ width: "90%" }}>
+  const toPoem = (href: string) => {
+    router.push(href);
+  };
+
+  const [show, setShow] = React.useState(true);
+  const onLoad = () => setShow(!show);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <Head>
+          <title>Poetry</title>
+          <meta
+            name="description"
+            content="A website created with the desire to be an artist."
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <div className={styles.main}>
           {entry.map((ent, index) => (
-            <List.Item key={index}>
-              <FlexboxGrid>
-                <FlexboxGrid.Item colspan={6}>
-                  {/* タイトルだけ親要素のフォントサイズの1.5倍で表示する */}
-                  <div style={{ fontSize: "1.5em" }}>
-                    <Link href={ent.link} passHref>
-                      <a>{ent.title}</a>
-                    </Link>
-                  </div>
-                  <div>{ent.date}</div>
-                </FlexboxGrid.Item>
-                <FlexboxGrid.Item colspan={18}>
-                  {ent.description}
-                  {typeof ent.reference === "string" && (
-                    <>
-                      <br />
-                      Reference:&emsp;
-                      <a
-                        href={ent.reference}
-                        style={{ fontSize: "1em!important" }}
-                      >
-                        {ent.reference}
-                      </a>
-                    </>
-                  )}
-                </FlexboxGrid.Item>
-              </FlexboxGrid>
-            </List.Item>
+            <div
+              className={styles.viewArea}
+              key={index}
+              onClick={() => toPoem(ent.link)}
+            >
+              <div className={styles.paragraph}>
+                <p style={{ fontSize: "1.5em" }}>{ent.title}</p>
+                <p style={{ width: "90%" }}>at: {ent.date}</p>
+                <hr style={{ width: "90%" }} />
+                <p style={{ width: "90%" }}>{ent.description}</p>
+              </div>
+            </div>
           ))}
-        </List>
+        </div>
       </div>
-    </div>
+      <IconButton
+        className={styles.button}
+        icon={<ArrowLeftLineIcon />}
+        onClick={toHome}
+        appearance="primary"
+        size="lg"
+        circle
+      />
+    </>
   );
 };
 
 export default Poetry;
+
+const styles = {
+  wrapper: css({
+    display: "flex",
+    height: "100vh",
+    width: "100vw",
+    justifyContent: "center",
+  }),
+
+  main: css({
+    overflow: "auto",
+    scrollSnapType: "y mandatory",
+    height: "100vh",
+    width: "100vw",
+  }),
+
+  viewArea: css({
+    display: "flex",
+    scrollSnapAlign: "start",
+    height: "50vh",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+  }),
+
+  paragraph: css({
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    height: "90%",
+    width: "90%",
+    backgroundColor: "rgba(15, 15, 15, 0.9)",
+  }),
+
+  button: css({
+    display: "fixed",
+    zIndex: "99999",
+    bottom: "7vh",
+    left: "2vw",
+    backgroundColor: "rgba(100, 100, 100, 0.7)",
+    overflow: "hidden",
+  }),
+};
